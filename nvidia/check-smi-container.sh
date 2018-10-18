0d78bc372d61bba6e8050ff47bcafacd9747fdb0
Zvonko Kosic <zkosic@redhat.com>
2018-10-18 07:18:00 -0400

    Added compiled module

--- nvidia/check-smi-container.sh
+++ nvidia/check-smi-container.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/bin/bash -x
 
 NGC="nvcr.io/nvidia/caffe:18.09-py2      \
      nvcr.io/nvidia/caffe2:18.08-py3     \
      nvcr.io/nvidia/cntk:18.08-py3       \
      nvcr.io/nvidia/cuda:latest          \
      nvcr.io/nvidia/mxnet:18.09-py3      \
-     nvcr.io/nvidia/pytorch:18.09-py3    \	
+     nvcr.io/nvidia/pytorch:18.09-py3    \
      nvcr.io/nvidia/tensorflow:18.09-py3 \
      nvcr.io/nvidia/theano:18.08         \
      nvcr.io/nvidia/torch:18.08-py2"
 
 
+
 for i in $NGC
 do
-    echo $i
+    _NGC_IMAGE=$(echo $i)
+    _NGC_POD_NAME=$(echo $_NGC_IMAGE | cut -d '/' -f3 | tr ':|.' '-')
+
+    oc exec -it ${_NGC_POD_NAME} nvidia-smi
+    
 done
 	 
 	 
--- nvidia/create-ngc-container.sh
+++ nvidia/create-ngc-container.sh
@@ -7,8 +7,13 @@ NGC="nvcr.io/nvidia/caffe:18.09-py2      \
      nvcr.io/nvidia/mxnet:18.09-py3      \
      nvcr.io/nvidia/pytorch:18.09-py3    \
      nvcr.io/nvidia/tensorflow:18.09-py3 \
+     nvcr.io/nvidia/tensorrt:18.09-py2   \
      nvcr.io/nvidia/theano:18.08         \
      nvcr.io/nvidia/torch:18.08-py2"
+
+NGC="nvcr.io/nvidia/digits:18.09"
+
+_NGC_DATA=$1
 
 
 for i in $NGC
@@ -19,6 +24,7 @@ do
     oc process -f ngc-framework-template.json \
        -p NGC_IMAGE=${_NGC_IMAGE}             \
        -p NGC_POD_NAME=${_NGC_POD_NAME}       \
+       -p NGC_DATA=${_NGC_DATA}               \
        | oc create -f -
     
 done
